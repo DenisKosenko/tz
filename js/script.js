d68e090e180b2bddc4516f20f30d87cc8c6adc81
DenisKosenko <denys27eu@gmail.com>
2021-03-26 04:34:59 +0200

    alpha

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 
 'use strict'
 
-// $(function(){
-//     $('.rev_slider').slick({
-//         dots: true,
-//         autoplay: true,
-//         responsive: [
-//             {
-//                 breakpoint: 993,
-//                 settings: {
-//                     arrows:false
-//                 }
-//             }
-//         ]
-//     });
-
-// })
-
-
 
