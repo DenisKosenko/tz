0b4939c8b0db66ccf2756c8af4d40f0b7a3045dd
DenisKosenko <denys27eu@gmail.com>
2021-03-26 05:52:05 +0200

    1.0.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 
 'use strict'
 
+let burger = document.querySelector('#burger')
+let menu = document.querySelector('#menu')
+let flag = false;
+
+burger.onclick = () => {
+    if(flag){
+        menu.style.left = '-500px';
+        flag = false
+    }else{
+        menu.style.left = '0';
+        flag = true
+    }
+}
 
